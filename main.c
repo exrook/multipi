bdc222f54bbe5c472ef0ce6ae0ff6059d80a7dd0
Jacob Hughes <exrook.j@gmail.com>
2013-05-13 01:05:10 -0400

    Sdded .gitignore file Added a description of the algorithm in main.c

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <gmp.h>
-mpz_t lol;
+
+//Algorithm to be used: 
+//             oo   <---- Infinity sign
+//           _____
+//           \
+// 1          \     (-1)^k * (6k)! * (13591409 + 545140134k)
+// -- = 12 *   *   ------------------------------------------
+// Pi         /         (3k)! * (k!)^3 * 640320^(3k+1.5)
+//           /____
+//            k=0
+
+mpz_t pi;
 void main() {
-  mpz_init(lol);
-  mpz_set_si(lol, 1);
-  long int i;
-  for(i=1;i<=20000;i++) {
-    mpz_mul_si(lol,lol,i);
-  }
-  printf(mpz_get_str(NULL,10,lol));
+  mpz_init(pi);
+  mpz_set_si(pi,0);
+  
+  printf(mpz_get_str(NULL,10,pi));
 }
